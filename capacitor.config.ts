c1a1492700aed3f83c1c7797e421804508a06914
sultanmyrza <sultanmyrza@gmail.com>
2022-11-24 15:32:14 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'stackblitz-ion6-ng14-blank-template',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
